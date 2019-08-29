40fe05d7da7279f84ed5c3c64ae95cb0583d2ddf
seallard <seallard95@gmail.com>
2019-08-29 15:46:08 -0700

    Get the largest contours

--- detect.py
+++ detect.py
@@ -23,24 +23,22 @@ def compare_images(img1, img2):
     plt.tight_layout()
     plt.show()
 
+
 if __name__ == '__main__':
     files = glob.glob('Additional_test_data/small/*')
     img = cv2.imread(files[30])
-    blurred_image = cv2.GaussianBlur(img, (5, 5), 0)
-    """
-    hsv = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2HSV)
-     # Every color except white
-    low = np.array([0, 42, 0])
-    high = np.array([179, 255, 255])
-    mask = cv2.inRange(hsv, low, high)
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
-    """
+
     imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    blurred_image = cv2.GaussianBlur(imgray, (5, 5), 0)
+    
     ret,thresh = cv2.threshold(imgray,127,255,0)
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    largest_contours = sorted_contours[:10]
+
     height, width = img.shape[:2]
     img_empty = np.zeros((height, width, 3))
-    cv2.drawContours(img_empty, contours, -1, (0, 255, 0), 3)
+    cv2.drawContours(img_empty, largest_contours, -1, (0, 255, 0), 3)
 
-    compare_images(img, img_empty)
+    compare_images(img, img_empty)    
