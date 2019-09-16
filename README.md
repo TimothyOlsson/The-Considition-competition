01a8c6e32cca58fd5ffa9615cf01fe5fea75c303
seallard <seallard95@gmail.com>
2019-09-17 00:30:49 +0200

    Load api key

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # The_Considition_competition
 https://theconsidition.se/competition
+
+Create api_key.py file with variable key="secret".
+Generate test model by running the notebook.
+
+To run model on new images from api, run main.py
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import api
+import api_key
 import solutionHelper
 import os
 import PIL
@@ -9,40 +10,51 @@ from fastai.callbacks.hooks import *
 # TODO: pip install requests
 # TODO: install fastai
 
-api_key = "YOUR-API-KEY-HERE"  # TODO: Enter your API key
+api_key = api_key.key
 
-_infer = load_learner('/PATH/TO/FOLDER/CONTAINING/MODEL/', 'model.pkl') # TODO: Load your trained model
+_infer = load_learner('./', 'trained_model.pkl') # TODO: Load your trained model
 
 def main():
+
     result = api.init_game(api_key)
+
     game_id = result["gameId"]
-    game_folder = '/PATH/TO/SAVE/GAME/game_' + game_id + '/'
+    game_folder = './games/game_' + game_id + '/'
     os.mkdir(game_folder)
+
     rounds_left = result['numberOfRounds']
     current_round = 1
+
     print("Starting a new game with id: " + game_id)
     print("The game has {} rounds and {} images per round".format(rounds_left, result["imagesPerRound"]))
+
     while rounds_left > 0:
+
         print("Starting new round, {} rounds left".format(rounds_left))
         zip_bytes = api.get_images(api_key)
         solutions = []
+
         # create folders for the current round of the game
         round_folder = game_folder + 'round_' + str(current_round) + '/'
         image_folder = round_folder + 'images/'
         output_folder = round_folder + 'output/'
+
         os.mkdir(round_folder)
         os.mkdir(image_folder)
         os.mkdir(output_folder)
 
         image_names = solutionHelper.save_images_to_disk(zip_bytes, image_folder)
+
         for name in image_names:
             solutions.append(analyze_image(name, image_folder, output_folder))
 
         solution_response = api.score_solution(api_key, {"Solutions": solutions})
         solutionHelper.print_errors(solution_response)
         solutionHelper.print_scores(solution_response)
+
         rounds_left = solution_response['roundsLeft']
         current_round += 1
+
 
 def analyze_image(name, image_folder, output_folder):  # Your image recognition function here
 
